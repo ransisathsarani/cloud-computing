8e31ca486df82c196d5448983c432c1a869cf270
ransi96 <ransi.sathsarani@betmakers.com>
2023-01-07 23:14:14 +0530

    minor fix

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-uf70gl618r4m/DemoApplication.war /usr/local/tomcat9/webapps/DemoApplication.wars
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-uf70gl618r4m/demo-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/demo-0.0.1-SNAPSHOT.jars
 
 
 # Ensure the ownership permissions are correct.
