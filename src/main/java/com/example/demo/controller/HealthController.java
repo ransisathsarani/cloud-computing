17cf5c34f534decec31d9b610fb9465a7a5c44c1
ransi96 <ransi.sathsarani@betmakers.com>
2023-01-08 13:27:10 +0530

    added builspec file and minor fixes

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    @GetMapping("/")
+    public ResponseEntity<?> ebHealth() {
+        return new ResponseEntity<>("Instance health check passed successfully", HttpStatus.OK);
+    }
+
     @GetMapping("/health")
     public ResponseEntity<?> health() {
         return new ResponseEntity<>("Success", HttpStatus.OK);
